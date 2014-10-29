57bbef16fe9dc838f8eab31c519d72d58d9b2e82
John Zhang <john.shimin.zhang@gmail.com>
2014-10-29 09:12:32 -0400

    Add backbone, fix usercontroller issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,9 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
     unless @user == current_user
-      flash[:msg] = "Thats not you!"
+      flash[:msg] = ["Thats not you!"]
       redirect_to root_url
     else
       @user = User.find(params[:id])
@@ -40,12 +41,12 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user != current_user
-      flash[:msg] = "Thats not you!"
+      flash[:msg] = ["Thats not you!"]
       redirect_to root_url
     elsif @user.update(user_params)
       redirect_to user_url(@user)
     else
-      flash[:msg] = @user.errors_full_messages
+      flash[:msg] = @user.errors.full_messages
       redirect_to user_url(@user)
     end
   end
@@ -53,12 +54,12 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     if @user != current_user
-      flash[:msg] = "Thats not you!"
+      flash[:msg] = ["Thats not you!"]
       redirect_to root_url
     elsif @user.destroy
       redirect_to users_url
     else
-      flash[:msg] = @user.errors_full_messages
+      flash[:msg] = @user.errors.full_messages
       redirect_to user_url(@user)
     end
   end
